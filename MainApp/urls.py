a0c2ad1485494ce8f30b3fc183a5eae37dc966e0
Marcus McCarty <marcus_mccarty1@baylor.edu>
2021-12-12 14:05:18 -0600

    Base page

---
+++ MainApp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.urls.resolvers import URLPattern
+
+#. means same folder
+
+from . import views
+
+app_name = 'MainApp'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
